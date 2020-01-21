6a8cb74b309deb0a167b377162984824eb961176
Gabriel Meloche <g.meloche@student.isartdigital.com>
2020-01-21 09:44:15 -0500

    Fixed issues in GPM (divisions by zero), created transform and camera

---
+++ Identity/Engine/include/Rendering/Camera.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <Export.h>
+
+namespace Engine::Rendering
+{
+    class API_ENGINE Camera
+    {
+    public:
+        Camera() = default;
+        ~Camera() = default;
+    };
+}
--- Identity/Engine/src/3DLoader/ObjectElements/Mesh.cpp
+++
@@ -1,7 +0,0 @@
-#include <stdafx.h>
-
-#include <3DLoader/ObjectElements/Mesh.h>
-
-Engine::ObjectElements::Mesh::Mesh(std::vector<Engine::Geometry::Vertex>& p_vertices, std::vector<uint32_t>& p_indices) : m_vertices{ p_vertices }, m_indices{ p_indices }
-{
-}
