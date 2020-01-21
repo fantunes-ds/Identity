3a3d7c2f8e5711746c5ee6edbfd6bbb227fbacf3
Lilian Gadeau <lilgado@hotmail.ca>
2020-01-21 14:08:26 -0500

    Changes in PixelShader

--- Identity/Test/src/main.cpp
+++ Identity/Test/src/main.cpp
@@ -6,5 +6,20 @@ int CALLBACK WinMain(
     LPSTR     lpCmdLine,
     int       nCmdShow)
 {
-    return Engine::Core::App{}.Run();
+    try
+    {
+        return Engine::Core::App{}.Run();
+    }
+    catch(const Engine::Tools::IdentityException& e)
+    {
+        MessageBox(nullptr, e.what(), e.GetType(), MB_OK | MB_ICONERROR);
+    }
+    catch(const std::exception& e)
+    {
+        MessageBox(nullptr, e.what(), "Standard Exception", MB_OK | MB_ICONERROR);
+    }
+    catch(...)
+    {
+        MessageBox(nullptr, "UNKNOWN ERROR", "UNKNOWN EXCEPTION", MB_OK | MB_ICONERROR);
+    }
 }
