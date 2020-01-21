ab173934b4505cbc4653a6ef949b5c7f70cd42ca
Gabriel Meloche <g.meloche@student.isartdigital.com>
2020-01-21 11:26:50 -0500

    3DLoader folder was accidentally put at wrong place in src

--- Identity/Engine/include/Rendering/Camera.h
+++ Identity/Engine/include/Rendering/Camera.h
@@ -1,12 +1,19 @@
 #pragma once
 #include <Export.h>
+#include <GPM/GPM.h>
 
 namespace Engine::Rendering
 {
     class API_ENGINE Camera
     {
     public:
-        Camera() = default;
+        Camera();
         ~Camera() = default;
+
+        void SetProjectionValues();
+
+    private:
+        void UpdateViewMatrix();
+        GPM::Vector3D m_position;
     };
 }
