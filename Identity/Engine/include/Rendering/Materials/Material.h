dffc40d47ef921bfb46f15998ea12c4848c28e54
Lilian Gadeau <l.gadeau@student.isartdigital.com>
2020-03-13 15:49:16 -0400

    Added comments in material.h file

--- Identity/Engine/include/Rendering/Materials/Material.h
+++ Identity/Engine/include/Rendering/Materials/Material.h
@@ -13,13 +13,48 @@ namespace Engine::Rendering::Materials
         Material() = default;
         ~Material() = default;
 
+        /**
+         * @brief Used to create a new material from a pixel shader, a vertex shader and a texture
+         * @param p_pixelShader The name of the pixel shader you want to use in the new material
+         * @param p_vertexShader The name of the vertex shader you want to use in the new material
+         * @return Return a share_ptr of the created material
+         */
         static std::shared_ptr<Material> CreateMaterial(const std::shared_ptr<PixelShader> p_pixelShader, const std::shared_ptr<VertexShader> p_vertexShader);
 
+        /**
+         * @breif Used to add a pixel shader to the current material
+         * @param p_device A pointer to the rendering device of the renderer (will soon be removed)
+         * @param p_path The path of the pixel shader you want to add to the current material
+         * @deprecated The Resource Manager will soon be available so prepare to make the change
+         */
         void AddPixelShader(const Microsoft::WRL::ComPtr<ID3D11Device>& p_device, const std::wstring& p_path);
+
+        /**
+         * @brief Used to add a vertex shader to the current material
+         * @param p_device A pointer to the rendering device of the renderer (will soon be removed)
+         * @param p_path The pat of the vertex shader you want to add to the current material
+         * @depracated The Resource Manager will soon be available so prepare to make the change
+         */
         void AddVertexShader(const Microsoft::WRL::ComPtr<ID3D11Device>& p_device, const std::wstring& p_path);
+
+        /**
+         * @brief Used to add a texture to the current material
+         * @param p_device A pointer to the rendering device of the renderer (will soon be removed)
+         * @param p_path The path to the texture you want to add to the current material
+         * @deprecated The Resource Manager will soon be available so prepare to make the change
+         */
         void AddTexture(const Microsoft::WRL::ComPtr<ID3D11Device>& p_device, const std::wstring& p_path);
 
+        /**
+         * @brief Used to bind the shaders, the constant buffer and the texture to the rendering context
+         * @param p_context A pointer to the device context of the renderer (will soon be removed)
+         */
         void Bind(const Microsoft::WRL::ComPtr<ID3D11DeviceContext>& p_context);
+
+        /**
+         * @brief Used to unbind the shaders, the constant buffers and the texture from the rendering context
+         * @param p_context A pointer to the device context of the renderer (will soon be removed)
+         */
         void Unbind(const Microsoft::WRL::ComPtr<ID3D11DeviceContext>& p_context);
 
         inline void SetPixelShader(const std::shared_ptr<PixelShader> p_pixelShader) { m_pixelShader = p_pixelShader; }
